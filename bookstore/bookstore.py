32beaf46f1355f82ff9a94006b0232eeee97eb93
Ashutosh Ahmad Alexandar <ashutoshahmadalexandar@Ashutoshs-MBP.fios-router.home>
2018-08-22 23:06:36 -0400

    Commit

---
+++ bookstore/bookstore.py
@@ -0,0 +1,61 @@
+def create_bookstore(name):
+    '''
+    bookstore = {
+        'name' : name,
+        'authors' : []
+        'books' : []
+    }
+    bookstore['name'] = name
+    bookstore['authors'] = []
+    bookstore['books'] = []
+    '''
+    return {
+        'name' : name,
+        'authors' : [],
+        'books' : []
+    }
+
+
+def get_bookstore_name(bookstore):
+    return bookstore['name']
+
+
+def add_author(bookstore, name, nationality):
+    author = {
+        'name' : name,
+        'nationality' : nationality
+    }
+    
+    bookstore['authors'].append(author)
+    
+    return author
+
+def get_author_by_name(bookstore, name):
+     for author in bookstore['authors']:
+         if name == author['name']:
+             return author
+
+
+def add_book(bookstore, title, isbn, author):
+    book = {
+        'title' : title,
+        'isbn' : isbn,
+        'author' : author
+    }
+    
+    bookstore['books'].append(book)
+    return book 
+
+
+def get_book_by_title(bookstore, title):
+    for book in bookstore['books']:
+        if book['title'] == title:
+            return book
+
+
+def get_books_by_author(bookstore, author):
+    books = []
+    for book in bookstore['books']:
+        if book['author'] == author:
+            books.append(book)
+    return books
